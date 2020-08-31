6034506b989f91bc9721d32d8956978a8cb20dd1
ktotheH <kcjhill1234@msn.com>
2020-08-31 17:41:25 -0600

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div
       style={{
-        backgroundImage: `url('/assets/fitness.jpg')`,
+        backgroundImage: ``,
         backgroundSize: "cover",
         overflow: "scroll",
         height: "100vh",
